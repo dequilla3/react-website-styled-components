3d9214cee7e535c9c91acd13f1072d7ba4f13db9
kadequilla <deqkim4@gmail.com>
2021-05-28 21:38:50 -1000

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Nav, NavbarContainer } from "./Navbar.elements";
+import { Container } from "../../GlobalStyle";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer />
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
